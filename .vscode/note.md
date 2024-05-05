9f191db96f2cdf76b02f421c984b3aae4572ee90
sueszli <jabaryyahya@gmail.com>
2024-05-05 16:47:42 +0200

    up

--- .vscode/note.md
+++
@@ -1,8 +0,0 @@
-make sure that prettier doesn't conflict with vscode.
-
-check your global `settings.json` file on vscode.
-
-```
-"prettier.tabWidth": 4,
-"editor.tabSize": 4,
-```
--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 if ! command -v python3 &> /dev/null; then echo "python3 missing"; exit 1; fi
 if ! command -v pip &> /dev/null; then echo "pip missing"; exit 1; fi
 
-python3 -m pip install --upgrade pip > /dev/null
+python3 -m pip install --upgrade pip
 
+# formatter
 pip install black > /dev/null
-pip install pipreqs > /dev/null
 
-rm -rf requirements.txt > /dev/null
-pipreqs . > /dev/null
-pip install -r requirements.txt > /dev/null
+# update requirements
+rm -rf requirements.txt
+pip install pipreqs
+pipreqs .
+
+# install requirements
+pip install -r requirements.txt
