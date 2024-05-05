787201ca9dcef1740764e12ec67136c0d888f480
sueszli <jabaryyahya@gmail.com>
2024-05-05 20:48:30 +0200

    put everything into the docker-compose.yml but it sucks

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,4 @@
-FROM alpine:latest
+FROM ubuntu:latest
 
-USER root
-
-# system packages
-RUN apk update && apk upgrade
-RUN apk add --no-cache \
-    bash wget curl jq git openssh \
-    python3 pipx
-
-# python packages -> cuda-torch can only be installed on linux
-# RUN pip install numpy pandas matplotlib seaborn
-# RUN pip install torch torchvision torchaudio
-
-# mount the current directory to the container, set as working directory
-VOLUME ["/code"]
-WORKDIR /code
-
-# entrypoint
-# CMD ["/bin/bash"]
+RUN apt-get update && apt-get install -y \
+    python3
--- run-docker.sh
+++ run-docker.sh
@@ -2,19 +2,18 @@
 docker-compose up
 
 # ------------------------------------------- use however you like
-docker ps --all
 code --attach-container <container_id>
 docker exec -it <container_id> /bin/bash
 
-# ------------------------------------------- stop
+# ------------------------------------------- stop, clean up
 docker-compose down
 
-# clean up
 ids=$(docker ps -a -q)
 for id in $ids; do docker stop $id; done
 for id in $ids; do docker rm $id; done
 docker ps --all
 
+# ------------------------------------------- remove everything
 docker rmi $(docker images -q)
 docker images
 
