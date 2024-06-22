34a2b63a73d9764b3fb2c7bcf24659879258f382
yahya jabary <61852663+sueszli@users.noreply.github.com>
2024-06-22 14:25:46 +0200

    Update run-local.sh

--- run-local.sh
+++ run-local.sh
@@ -1,6 +1,3 @@
-if ! command -v python3 &> /dev/null; then echo "python3 missing"; exit 1; fi
-if ! command -v pip &> /dev/null; then echo "pip missing"; exit 1; fi
-
 python3 -m pip install --upgrade pip
 
 # find out dependencies
