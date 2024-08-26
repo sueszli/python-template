3b3c9f75081a87fce5064f9d42945cd7ff5fc2dd
yahya jabary <61852663+sueszli@users.noreply.github.com>
2024-08-27 01:57:12 +0200

    Create alive.sh

---
+++ alive.sh
@@ -0,0 +1,31 @@
+python_file="$PWD/src/mnist.py"
+
+# stay alive
+monitor() {
+    while true; do
+        if ! pgrep -f "$python_file" > /dev/null; then
+            echo "$(date): process died, restarting..." >> alive-monitor.log
+            python3 "$python_file" >> "alive.log" 2>&1 &
+            echo $! > "alive.pid"
+        fi
+        sleep 5
+    done
+}
+monitor >> "alive-monitor.log" 2>&1 &
+echo $! > "alive-monitor.pid"
+echo "$(date): started" >> "alive-monitor.log"
+
+# watch
+watch -n 0.1 "tail -n 100 alive.log"
+while true; do clear; tail -n 100 alive.log; sleep 0.1; done
+pgrep -f "$python_file"
+nvtop
+htop
+
+# kill
+kill $(cat "alive.pid")
+rm -f alive.log
+rm -f alive.pid
+kill $(cat "alive-monitor.pid")
+rm -f alive-monitor.log
+rm -f alive-monitor.pid
